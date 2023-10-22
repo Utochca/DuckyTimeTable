b23e63f752c8ae35f1dd2bd8133caaf8e9c62718
Utochca <petechka.poddubnyy@gmail.com>
2023-10-22 21:56:08 +0300

    test

--- app/src/main/java/com/android/duckytimetable/MainActivity.kt
+++ app/src/main/java/com/android/duckytimetable/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    //hello 123
     //Hello qwe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
