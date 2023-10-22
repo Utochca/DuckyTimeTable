599e4e89b28dfead47d3f89f3cdfa8e27a121d0a
Utochca <petechka.poddubnyy@gmail.com>
2023-10-22 20:37:57 +0300

    test

--- app/src/main/java/com/android/duckytimetable/MainActivity.kt
+++ app/src/main/java/com/android/duckytimetable/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    //Hello utochka!
+    //Hello qwe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
