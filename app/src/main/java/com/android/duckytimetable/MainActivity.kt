15bd07573ef277b057106d0102b5af3dcc8f0692
ppk4n <rubytimer@gmail.com>
2023-10-22 22:03:35 +0300

    prsojtih;orjoih;srt

--- app/src/main/java/com/android/duckytimetable/MainActivity.kt
+++ app/src/main/java/com/android/duckytimetable/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    //hello 12
-    //Hello qwe
+    //hello 123
+    //123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
