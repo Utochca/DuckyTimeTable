1e1eeee1e2100f358563a612376152ef6bf3c614
ppk4n <rubytimer@gmail.com>
2023-10-22 22:05:28 +0300

    prsojtih;orjoih;srt

--- app/src/main/java/com/android/duckytimetable/MainActivity.kt
+++ app/src/main/java/com/android/duckytimetable/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    //hello 123
+    //hello
     //123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
