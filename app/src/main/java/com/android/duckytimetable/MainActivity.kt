37b2b7be22e67aa088277c01a4d1d67a6564ede4
Utochca <petechka.poddubnyy@gmail.com>
2023-10-22 22:12:33 +0300

    test

--- app/src/main/java/com/android/duckytimetable/MainActivity.kt
+++ app/src/main/java/com/android/duckytimetable/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     //lezdoit
+    //cockandballs
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
