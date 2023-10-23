ea7f07970f3f8cb8dbff9ee1d9ec1adcc79569f8
Utochca <petechka.poddubnyy@gmail.com>
2023-10-23 19:46:15 +0300

    test

--- app/src/main/java/com/android/duckytimetable/MainActivity.kt
+++ app/src/main/java/com/android/duckytimetable/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.android.duckytimetable
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
 
-    //lezdoit
-    //cockandballs
+    private var toolbar: Toolbar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar?.title = ""
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add) {
+            Toast.makeText(this, "Clicked Add Icon..", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
