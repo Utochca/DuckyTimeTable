ac0fdeea59b02bd633b776a877beffea6050ede3
Utochca <petechka.poddubnyy@gmail.com>
2023-12-23 20:31:51 +0300

    qwe

--- app/src/main/java/com/android/duckytimetable/MyApp.kt
+++ app/src/main/java/com/android/duckytimetable/MyApp.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.media.AudioAttributes
+import android.net.Uri
 import android.os.Build
 
 class MyApp : Application() {
@@ -20,6 +22,11 @@ class MyApp : Application() {
                 NotificationManager.IMPORTANCE_DEFAULT
             )
             channel.description = "Used for notifications"
+
+            val attributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .build()
+            channel.setSound(Uri.parse("android.resource://" + packageName + "/" + R.raw.quack), attributes)
 
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
